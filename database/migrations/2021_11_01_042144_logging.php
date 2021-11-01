c4eaa52c6a6e9ba8ca0b046fd390ae00d2013f46
SirRana <60571003+RanaIsHere@users.noreply.github.com>
2021-11-01 12:11:02 +0700

    trigger test experimentation

--- database/migrations/2021_11_01_042144_logging.php
+++ database/migrations/2021_11_01_042144_logging.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class Logging extends Migration
@@ -18,6 +19,13 @@ class Logging extends Migration
             $table->string('action', 20);
             $table->timestamps();
         });
+
+        DB::unprepared(DB::raw("
+            CREATE TRIGGER sellLog AFTER INSERT 
+            ON users FOR EACH ROW BEGIN 
+                INSERT INTO logging (action, created_at, updated_at) VALUES ('INSERT', CURRENT_TIME(), CURRENT_TIME());
+            END;
+        "));
     }
 
     /**
