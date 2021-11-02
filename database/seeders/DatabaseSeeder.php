8d84fa147da58e999ba188c462ccd9947c1abce7
SirRana <60571003+RanaIsHere@users.noreply.github.com>
2021-11-02 11:23:20 +0700

    Factory created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Patrons;
+use App\Models\Suppliers;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -19,12 +20,31 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(32)->create();
         Patrons::factory(12)->create();
+        Suppliers::factory(6)->create();
 
         DB::table('users')->insert([
             'name' => 'team_origin',
             'email' => 'team_origin@protonmail.com',
             'password' => Hash::make('admin'),
             'level' => 'ADMIN',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Zarythex',
+            'email' => 'zarythex@protonmail.com',
+            'password' => Hash::make('admin'),
+            'level' => 'EDP',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Grempoots2',
+            'email' => 'grempoots2@protonmail.com',
+            'password' => Hash::make('admin'),
+            'level' => 'OPERATOR',
             'created_at' => now(),
             'updated_at' => now()
         ]);
